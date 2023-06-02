e1ccd32a1426c02de3b624444b57d3c016a061ac
Nick Hamilton <nicholashamilton.co@gmail.com>
2023-06-01 22:23:54 -0400

    Adds jest application tests

---
+++ src/index.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest';
+import App from './app';
+
+const app = new App();
+
+describe('Test Application', function () {
+    test('GET /api', (done) => {
+        request(app.getApplication())
+            .get('/api')
+            .expect(200, {
+                message: 'success',
+            })
+            .end((err) => {
+                if (err) return done(err);
+                return done();
+            });
+    });
+});
